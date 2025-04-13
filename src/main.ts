7799177da8d6da9786e5e89a8feae11d6307a41e
Kjell-Arne <kjell-arne@neshagen.no>
2025-04-13 21:28:42 +0200

    Starter done.. Let the projects start..

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PermissionCacheService } from './auth/services/permission-cache.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
+
+  if (process.env.NODE_ENV === 'development') {
+    const permissionCache = app.get(PermissionCacheService);
+    permissionCache.logCacheState(true);
+  }
 }
 bootstrap();
